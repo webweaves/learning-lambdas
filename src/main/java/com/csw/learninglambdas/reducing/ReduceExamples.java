3fe28a3c7695d7315e2123188b623e6d5605fd23
csw <christianweaves@gmail.com>
2022-03-22 23:45:55 +0000

    WIP

--- src/main/java/com/csw/learninglambdas/reducing/ReduceExamples.java
+++ src/main/java/com/csw/learninglambdas/reducing/ReduceExamples.java
@@ -6,23 +6,55 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
+/**
+ * reducing is the repeated process of combining all elements
+ */
 public class ReduceExamples {
 
     Function<Integer, BigDecimal> convertToBigDecimal = anInt -> new BigDecimal(anInt);
 
-    public Optional<BigDecimal> reduceAgesWithOptional(List<Person> peopleList) {
+    /**
+     * stream the list of people, map a person to their age and
+     * convert the age to a big decimal using the convertToBigDecimal function defined above
+     * then reduce (sum) all the elements in the list.
+     * <p>
+     * The reduce call returns an Optional
+     *
+     * @param peopleList
+     * @return Optional
+     */
+    public Optional<BigDecimal> totalAgesWithOptional(List<Person> peopleList) {
         return peopleList.stream()
                 .map(Person::getAge)
                 .map(convertToBigDecimal)
                 .reduce(BigDecimal::add);
     }
 
-
-    public BigDecimal reduceAgesNoNeedForOptional(List<Person> peopleList) {
+    /**
+     * a repeat of the example above except provide the reduce call with an identity, see the API documentation
+     *
+     * @param peopleList
+     * @return BigDecimal
+     */
+    public BigDecimal totalAgesNoNeedForOptional(List<Person> peopleList) {
         return peopleList.stream()
                 .map(Person::getAge)
                 .map(convertToBigDecimal)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    /**
+     * work through the list of people from a specific country and concatenate their names and return
+     *
+     * @param peopleList
+     * @return
+     */
+    public String combinePeoplesNames(List<Person> peopleList) {
+        return peopleList.stream()
+                .filter(p -> p.getCountry().equals("Wales"))
+                .map(Person::getName)
+                .collect(Collectors.joining(", "));
     }
 }
--- src/test/java/com/csw/learninglambdas/reducing/ReduceExamplesTest.java
+++ src/test/java/com/csw/learninglambdas/reducing/ReduceExamplesTest.java
@@ -9,27 +9,52 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ReduceExamplesTest {
+
+    final List<Person> people = SimplePersonFactory.createPeople();
 
     @Test
     public void calculatePeopleAges_items() {
-        List<Person> people = SimplePersonFactory.createPeople();
-        outputOptional(new ReduceExamples().reduceAgesWithOptional(people));
+        outputOptional(new ReduceExamples().totalAgesWithOptional(people));
     }
 
     @Test
     public void calculatePeopleAges_no_items() {
-        List<Person> people = new ArrayList<>();
-        outputOptional(new ReduceExamples().reduceAgesWithOptional(people));
+        List<Person> noPeople = new ArrayList<>();
+        outputOptional(new ReduceExamples().totalAgesWithOptional(noPeople));
     }
 
     @Test
-    public void calculatePeopleAges_no_need_for_optional() {
+    public void calculatePeopleAges_no_need_for_optional_no_data() {
+        /**
+         * call the reduce method with an empty stream, the identity value should be returned
+         */
         List<Person> people = new ArrayList<>();
-        BigDecimal count = new ReduceExamples().reduceAgesNoNeedForOptional(people);
+        BigDecimal count = new ReduceExamples().totalAgesNoNeedForOptional(people);
+        assertEquals(0, Integer.parseInt(count.toString()));
+        System.out.println("Total age:" + count);
     }
 
-    public void outputOptional(Optional optional) {
+    @Test
+    public void calculatePeopleAges_no_need_for_optional_with_data() {
+        /**
+         * call the reduce method with an empty stream, the identity value should be returned
+         */
+        BigDecimal count = new ReduceExamples().totalAgesNoNeedForOptional(people);
+        assertEquals(370, Integer.parseInt(count.toString()));
+        System.out.println("Total age:" + count);
+    }
+
+    @Test
+    public void concatenateWelshPeoplesNames() {
+        String concatenateWelshNames = new ReduceExamples().combinePeoplesNames(people);
+        assertEquals("Dave, Soloman", concatenateWelshNames);
+        System.out.println(concatenateWelshNames);
+    }
+
+    private void outputOptional(Optional optional) {
         optional.ifPresentOrElse(
                 total -> System.out.printf("Total age count = %s", total),
                 () -> System.out.println("No ages to calculate")
